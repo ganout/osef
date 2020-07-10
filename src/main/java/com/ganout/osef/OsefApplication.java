acaac3367768c0d7caf52bb75e5cc1858e683f28
ganout <morgan.bourdillel@gmail.com>
2020-07-10 18:15:13 +0200

    init

---
+++ src/main/java/com/ganout/osef/OsefApplication.java
@@ -0,0 +1,13 @@
+package com.ganout.osef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ganout/osef/OsefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganout.osef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
